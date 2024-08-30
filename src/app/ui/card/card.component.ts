0afc9db224ed2c308f64712763805fc274edbde8
Christophe Mancini <mancini.christophe@gmail.com>
2024-08-30 15:27:42 +0200

    ui card component

---
+++ src/app/ui/card/card.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Output, output } from '@angular/core';
+import { CloseButtonComponent } from "../button/close-button/close-button.component";
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [CloseButtonComponent, CloseButtonComponent],
+  template: `<div id="card">
+    <app-close-button (click)="handleclose()"></app-close-button>
+    <ng-content select=".title"></ng-content>
+    <ng-content select=".truc"></ng-content>
+  </div>`,
+  styles: `#card{
+            background-color: #ffffff;
+            padding: 30px;
+            border-radius: 10px;
+            box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.25);
+          }
+
+          app-close-button{
+            position: absolute;
+            top: 5px;
+            right: 5px;
+          }`,
+})
+export class CardComponent {
+  @Output() clickClose = new EventEmitter();
+
+  handleclose() {
+    console.log('handleclose');
+    this.clickClose.emit();
+  }
+}
