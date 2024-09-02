4aab4b4fefd3e79117188d1a3e8bbb98576a606f
Christophe Mancini <mancini.christophe@gmail.com>
2024-09-02 19:17:42 +0200

    fix-  display photo & default in list & grid

--- src/app/housing-location-grid/housing-location-grid.component.ts
+++ src/app/housing-location-grid/housing-location-grid.component.ts
@@ -10,50 +10,62 @@ import { Queryoptions } from '../queryoptions';
   standalone: true,
   imports: [RouterModule, LocationDetailsComponent],
   template: `
-  <div>
-    <div class="orderBy">
-      <select name="orderBy" ngModel (change)="orderBy($event)">
-        <option value="">Order by</option>
-        <option value="name">Name</option>
-        <option value="city">City</option>
-        <option value="state">State</option>
-        <option value="availableUnits">Available Units</option>
-      </select>
-    </div>
-    <section class="grid">
-      @for(housingLocation  of housingList; track housingList){
-      <div class="housing-location-card" [routerLink]="['details',housingLocation.id]">
-        <img  class="housing-photo" src="{{housingLocation.photos}}"/>
-        <div class="details">
-          <h2>{{housingLocation.name}}</h2>
-          <app-location-details [city]="housingLocation.city" [state]="housingLocation.state"/>
-          <!-- <a class="primary-link" [routerLink]="['details',housingLocation.id]">See details</a>   -->
-          <a class="edit-btn secondary" [routerLink]="['edit',housingLocation.id]">Edit</a>
-        </div>
+    <div>
+      <div class="orderBy">
+        <select name="orderBy" ngModel (change)="orderBy($event)">
+          <option value="">Order by</option>
+          <option value="name">Name</option>
+          <option value="city">City</option>
+          <option value="state">State</option>
+          <option value="availableUnits">Available Units</option>
+        </select>
       </div>
-      }
-    </section>
-  </div>
+      <section class="grid">
+        @for(housingLocation of housingList; track housingList){
+        <div
+          class="housing-location-card"
+          [routerLink]="['details', housingLocation.id]"
+        >
+          <img class="housing-photo" 
+            src="{{
+              housingLocation.photos && housingLocation.photos[0]
+                ? housingLocation.photos[0]
+                : 'assets/3926921.png'
+            }}"/>
+          <div class="details">
+            <h2>{{ housingLocation.name }}</h2>
+            <app-location-details
+              [city]="housingLocation.city"
+              [state]="housingLocation.state"
+            />
+            <!-- <a class="primary-link" [routerLink]="['details',housingLocation.id]">See details</a>   -->
+            <a
+              class="edit-btn secondary"
+              [routerLink]="['edit', housingLocation.id]"
+              >Edit</a
+            >
+          </div>
+        </div>
+        }
+      </section>
+    </div>
   `,
-  styleUrl: './housing-location-grid.component.css'
+  styleUrl: './housing-location-grid.component.css',
 })
-export class HousingLocationGridComponent{
-  @Input() housingList!: HousingLocation[]
-  @Output() onOrderBy = new EventEmitter<any>()
-  constructor(private housingService: HousingService) {
-    
-  }
+export class HousingLocationGridComponent {
+  @Input() housingList!: HousingLocation[];
+  @Output() onOrderBy = new EventEmitter<any>();
+  constructor(private housingService: HousingService) {}
 
-  orderBy(event:any) {
-    console.log(event.target.value)
-    let param :Queryoptions = {}
+  orderBy(event: any) {
+    console.log(event.target.value);
+    let param: Queryoptions = {};
     // if(event.target.value == '') this.housingService.clearOrdering()
     // else param = { order: { order: 'ASC', by: event.target.value } }
-    this.onOrderBy.emit(param)
+    this.onOrderBy.emit(param);
     // this.housingService.getHousingLocationList(param)
     //   .then((reponse) => {
     //     this.housingList = reponse
     // })
   }
-
 }
