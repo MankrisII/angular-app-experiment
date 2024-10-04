34aabcbd24bb70178de3181355208e63f464b568
Christophe Mancini <mancini.christophe@gmail.com>
2024-10-04 19:01:13 +0200

    Add child routes : grid, list, map

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,34 @@ import { HomeComponent } from './home/home.component';
 import { HousingDetailsComponent } from './housing-details/housing-details.component';
 import { HousingEditComponent } from './housing-edit/housing-edit.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HousingLocationGridComponent } from './housing-location-grid/housing-location-grid.component';
+import { HousingLocationListComponent } from './housing-location-list/housing-location-list.component';
+import { HousingLocationMapComponent } from './housing-location-map/housing-location-map.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Home',
+    children: [
+      {
+        path: 'grid',
+        component : HousingLocationGridComponent
+      },
+      {
+        path: 'list',
+        component : HousingLocationListComponent
+      },
+      {
+        path: 'map',
+        component : HousingLocationMapComponent
+      },
+      {
+        path: '',
+        redirectTo: '/list',
+        pathMatch: 'full',
+      }
+    ]
   },
   {
     path: 'details/:id',
