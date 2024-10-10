3b06e82c4f1abaeeb97fe0ee2051db5cea5f8ccb
Christophe Mancini <mancini.christophe@gmail.com>
2024-10-10 08:30:32 +0200

    refact close button

--- src/app/ui/button/close-button/close-button.component.ts
+++ src/app/ui/button/close-button/close-button.component.ts
@@ -4,9 +4,44 @@ import { Component } from '@angular/core';
   selector: 'app-close-button',
   standalone: true,
   imports: [],
-  templateUrl: './close-button.component.html',
-  styleUrl: './close-button.component.css'
-})
-export class CloseButtonComponent {
+  template: `<a class="closeButton"></a>`,
+  styles: `.closeButton {
+              display: block;
+              position: relative;
+              background-color: var(--secondary-color);
+              border-radius: 100px;
+              width: 20px;
+              height: 20px;
+              /* top: 8px;
+              right: 94px; */
+              color: rgb(86, 78, 194);
+              cursor: pointer;
+              text-decoration: none;
+            }
 
-}
+            .closeButton::after,
+            .closeButton::before {
+              content: "";
+              display: block;
+              position: absolute;
+            }
+
+            .closeButton::after {
+              border-bottom: 2px solid var(--primary-color);
+              width: 12px;
+              height: 2px;
+              rotate: 45deg;
+              left: 5px;
+              top: 7px;
+            }
+
+            .closeButton::before {
+              border-left: 2px solid var(--primary-color);
+              width: 2px;
+              height: 12px;
+              rotate: -135deg;
+              left: 7px;
+              top: 3px;
+            }`,
+})
+export class CloseButtonComponent {}
