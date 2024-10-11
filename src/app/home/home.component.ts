10e68eb7f78d66bad060e77692da76f6314a90da
Christophe Mancini <mancini.christophe@gmail.com>
2024-10-11 19:52:46 +0200

    Using AddressInputComp for search input

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, ViewChild, inject } from '@angular/core';
 import { HousingLocation } from '../HousingLocation';
 import { HousingLocationGridComponent } from '../housing-location-grid/housing-location-grid.component';
 import { HousingService } from '../housing.service';
@@ -6,11 +6,18 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HousingLocationListComponent } from '../housing-location-list/housing-location-list.component';
 import { HomeCustomisationService } from './home-customisation.service';
 import { NgClass, NgStyle } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { FirebaseService } from '../firebase.service';
 import { SorterService } from '../list-sorter-heading/sorter.service';
 import { CloseButtonComponent } from '../ui/button/close-button/close-button.component';
 import { HousingLocationMapComponent } from '../housing-location-map/housing-location-map.component';
+import { AddressInputComponent } from '../housing-edit/address-input/address-input.component';
 
 @Component({
   selector: 'app-home',
@@ -20,23 +27,29 @@ import { HousingLocationMapComponent } from '../housing-location-map/housing-loc
     HousingLocationListComponent,
     HousingLocationMapComponent,
     RouterLink,
+    ReactiveFormsModule,
     RouterLinkActive,
     NgClass,
     FormsModule,
     NgStyle,
     CloseButtonComponent,
     RouterOutlet,
+    AddressInputComponent,
   ],
   styleUrl: './home.component.css',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
   housingList: HousingLocation[] = [];
-  searchInput: string = '';
   firebaseService = inject(FirebaseService);
   housingService = inject(HousingService);
   customisationService = inject(HomeCustomisationService);
   sorterService = inject(SorterService);
+  formBuilder = inject(FormBuilder);
+  @ViewChild('search', { static: true }) searchInput!: AddressInputComponent;
+  searchForm = this.formBuilder.group({
+    search: ['', Validators.required],
+  });
 
   constructor() {}
 
@@ -57,7 +70,7 @@ export class HomeComponent implements OnInit {
   }
 
   clearSearch() {
-    this.searchInput = '';
+    this.searchForm.controls['search'].setValue('');
     this.housingService.clearSearch();
     // this.sorterService.clearOrder()
   }
